9ec6de544e7ebd6892caca5b541416544309da87
lkotlarenko <lkotlarenko@protonmail.com>
2021-10-25 13:27:26 -0300

    Refactorando JS e adicionando funcao para limpar quadro

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const pixelBoard = 25;
+const defaultBoard = 25;
+const boardLocation = document.getElementById('pixel-board');
 
-window.onload = function () {
-  const boardLocation = document.getElementById('pixel-board');
-  for (let i = 0; i < pixelBoard; i += 1) {
-    const pixel = document.createElement('li');
-    pixel.className = 'pixel';
-    boardLocation.appendChild(pixel);
+function resetBoard() {
+  const boardPixels = document.querySelectorAll('.pixel');
+  const boardLength = Object.keys(boardPixels).length;
+  if (boardLength >= 5) {
+    for (let i = 0; i < boardLength; i += 1) {
+      boardLocation.removeChild(boardPixels[i]);
+    }
   }
-};
+}
+
+function generateBoard(size) {
+  if (size < 5 || size > 25) {
+    alert('Wrong board size!');
+  } else {
+    resetBoard();
+    for (let i = 0; i < size; i += 1) {
+      const pixel = document.createElement('li');
+      pixel.className = 'pixel';
+      boardLocation.appendChild(pixel);
+    }
+  }
+}
+
+window.onload = generateBoard(defaultBoard);
